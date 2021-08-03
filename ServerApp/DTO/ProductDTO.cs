77109262a45cfa733aeb334c56f9ee3d1fc43613
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2021-08-03 19:49:21 +0300

    3.08.2021

---
+++ ServerApp/DTO/ProductDTO.cs
@@ -0,0 +1,10 @@
+namespace ServerApp.DTO
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ServerApp/Models/Role.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ServerApp.Models
+{
+    public class Role : IdentityRole<int>
+    {
+    }
+}
